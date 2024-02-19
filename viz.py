a9f56dcc83f9e05d9df2d33318bf68a95041dd97
namjmnam <namjmnam@naver.com>
2024-02-19 12:00:30 +0900

    Added triangles to output obj

--- viz.py
+++ viz.py
@@ -59,19 +59,6 @@ def get_faces(file_path):
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
 obj_file = filedialog.askopenfilename(filetypes=[("OBJ files", "*.obj")], initialdir=script_directory)
-obj_output = filedialog.asksaveasfilename(defaultextension='.obj',
-                                        filetypes=[('OBJ files', '*.obj')],
-                                        title='Save output file as',
-                                        initialfile=f'output.obj',
-                                        initialdir=script_directory)
-
-# Delete all lines that contain "nan"
-with open(obj_file, 'r') as input_file, open(obj_output, 'w') as output_file:
-    # Iterate over each line in the original file
-    for line in input_file:
-        # If "nan" is not found in the line, write it to the new file
-        if "nan" not in line and "f" not in line:
-            output_file.write(line)
 
 # Initialize an empty list to hold the line numbers
 nan_vertices = []
@@ -97,8 +84,16 @@ filtered_faces = [f for f in faces if not any(v in nan_vertices for v in f)]
 # print(filtered_faces)
 
 # print("Faces with nans but not all are nans")
-# edge_faces = [f for f in faces if any(v in nan_vertices for v in f) and not all(v in nan_vertices for v in f)]
+edge_faces = [f for f in faces if any(v in nan_vertices for v in f) and not all(v in nan_vertices for v in f)]
 # print(edge_faces)
+
+print("From edge faces, all nan vertices removed")
+cleaned_faces = []
+for f in edge_faces:
+    # Remove values that are in the first list
+    cleaned_list = [value for value in f if value not in nan_vertices]
+    cleaned_faces.append(cleaned_list)
+print(cleaned_faces)
 
 # print("Subtracted and corrected faces with nans eliminated")
 modified_faces = []
@@ -113,8 +108,41 @@ for lst in filtered_faces:
     modified_faces.append(modified_list)
 # print(modified_faces)
 
-# Add new face lines to the output file from above
-with open(obj_output, 'a') as output_file:
+# print("Subtracted and corrected cleaned faces with nans eliminated")
+modified_cleaned_faces = []
+for lst in cleaned_faces:
+    modified_list = []
+    for value in lst:
+        # Count the number of numbers in the first list that are smaller than the current value
+        count_smaller = sum(1 for num in nan_vertices if num < value)
+        # Subtract the count from the value
+        modified_value = value - count_smaller
+        modified_list.append(modified_value)
+    modified_cleaned_faces.append(modified_list)
+# print(modified_faces)
+
+# Write to the output file
+obj_output = filedialog.asksaveasfilename(defaultextension='.obj',
+                                        filetypes=[('OBJ files', '*.obj')],
+                                        title='Save output file as',
+                                        initialfile=f'output.obj',
+                                        initialdir=script_directory)
+
+# Delete all lines that contain "nan"
+with open(obj_file, 'r') as input_file, open(obj_output, 'w') as output_file:
+    # Iterate over each line in the original file
+    for line in input_file:
+        # If "nan" is not found in the line, write it to the new file
+        if "nan" not in line and "f" not in line:
+            output_file.write(line)
+
+    # Add new face lines
     for f in modified_faces:
         # print('f '+' '.join(str(num) for num in f)) 
         output_file.write('f '+' '.join(str(num) for num in f)+'\n')
+
+    # Add triangles from cleaned faces
+    # Needs improvement, because it should not add bottomleft-topright triangles?
+    for f in modified_cleaned_faces:
+        if len(f)>=3:
+            output_file.write('f '+' '.join(str(num) for num in f)+'\n')
