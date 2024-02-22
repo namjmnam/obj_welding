8cec864175fad85aed0c24ff8d96c385ea3b8bf8
namjmnam <namjmnam@naver.com>
2024-02-22 11:05:51 +0900

    Fixed issue with triangles

--- edge_writer.py
+++ edge_writer.py
@@ -7,21 +7,21 @@ from package.convert import Converter
 from package.weld import Welder
 import time
 
-ceil = Converter()
+a = Converter()
 b = Welder()
 
 # Get the script directory
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Needed for triangles
-xMax = 500
+xMax = 50
 
 # Start the timer
 start_time = time.time()
 
 print("Applying triangles algorithm...")
 # Now, triangles algorithm
-for f in ceil.cleaned_faces:
+for f in a.cleaned_faces:
     if len(f)==3:
         # 좌상/우상
         if f[1]-f[0]==1:
@@ -46,6 +46,30 @@ for f in ceil.cleaned_faces:
 end_time = time.time()
 time_taken = end_time - start_time
 print(f"Time taken for applying: {time_taken} seconds")
+# Start the timer
+start_time = time.time()
+
+print("Correcting face indices to remove nan...")
+# print("Subtracted and corrected cleaned faces with nans eliminated")
+modified_cleaned_faces = nan_correct(a.cleaned_faces, a.nan_vertices)
+# print(modified_cleaned_faces)
+
+# End the timer
+end_time = time.time()
+time_taken = end_time - start_time
+print(f"Time taken for correcting: {time_taken} seconds")
+# Start the timer
+start_time = time.time()
+
+print("Creating list of edge vertices...")
+# print("Vertices located on the edge")
+edge_vertices = sorted(set(sum(modified_cleaned_faces, [])))
+# print(edge_vertices)
+
+# End the timer
+end_time = time.time()
+time_taken = end_time - start_time
+print(f"Time taken for creating: {time_taken} seconds")
 
 # Write to the output file
 obj_output = filedialog.asksaveasfilename(defaultextension='.obj',
@@ -65,7 +89,7 @@ with open(b.combined_file, 'r') as target, open(obj_output, 'w') as output:
         output.write(line)
 
     # Add edge lines
-    for v in ceil.edge_vertices:
+    for v in edge_vertices:
         output.write(' '.join(['l', str(v), str(v+b.floor_v_count)])+'\n')
 
 # End the timer
